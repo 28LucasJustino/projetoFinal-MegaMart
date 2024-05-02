b750bb2b0f59ffb315cbf815538a401eb595b4a0
28LucasJustino <lucasjust28@gmail.com>
2024-05-02 17:30:43 -0300

    jjjjjjjjjjjjjj

--- src/java/model/DAO/ProdutoDAO.java
+++ src/java/model/DAO/ProdutoDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model.DAO;
 
 import conexao.Conexao;
@@ -14,11 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 import model.bean.ProdutoDTO;
 
-
-/**
- *
- * @author Marce
- */
 public class ProdutoDAO {
    public List<ProdutoDTO> read() {
         List<ProdutoDTO> prod = new ArrayList();
